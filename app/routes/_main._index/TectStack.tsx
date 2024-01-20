a74f1cdf5066e97f0cb3e24e524d8b2d7af1195b
Daniyal Ahmad Rizaldhi <daniyal.ahmadrizaldhi@gmail.com>
2024-01-20 19:55:15 +0700

    add techstack info with static content

---
+++ app/routes/_main._index/TectStack.tsx
@@ -0,0 +1,23 @@
+interface TechItemProps {
+    children: React.ReactNode
+}
+
+const TechItem = ({ children }: TechItemProps) => {
+    return (
+        <div className="bg-foreground text-background p-2 font-bold">
+            {children}
+        </div>
+    )
+}
+
+
+interface TechstackProps {
+    items: string[]
+}
+export default function Techstack({ items }: TechstackProps) {
+    return (
+        <div className="flex gap-2 flex-wrap">
+            {items.map((v, i) => <TechItem key={i}>{v}</TechItem>)}
+        </div>
+    )
+}
--- app/routes/_main._index/route.tsx
+++ app/routes/_main._index/route.tsx
@@ -1,27 +1,56 @@
 import type { MetaFunction } from "@remix-run/node";
 import Intro from "./Intro";
+import Techstack from "./TectStack";
 
 export const meta: MetaFunction = () => {
     return [
         { title: "Salty Project" },
-        { name: "description", content: "The World Is Saltier Than You Thought" },
+        { name: "description", content: "Daniyal's personal site with a sprinkle of salt." },
     ];
 };
+
+const dailyTechStack = [
+    "Typescript/Javascript",
+    "ReactJS",
+    "Next.JS",
+    "Remix",
+    "ExpressJS",
+    "Hono",
+    "Rust",
+    "Tailwindcss"
+]
+
+const recentTechStack = [
+    "Postgres",
+    "SQLite",
+    "MySQL/MariaDB",
+    "MongoDB",
+    "Firebase",
+    "Docker",
+    "Vue.js",
+    "GraphQL",
+    "GraphQL Yoga",
+    "Hasura",
+    "WebRTC",
+    "Websocket"
+]
 
 export default function Index() {
     return (
         <div className="pt-[10rem]">
             <div className="container mx-auto">
                 <Intro />
-            </div>
-            <div className="h-dvh flex flex-col text-slate-600">
-                <div className="flex-grow flex flex-col items-center justify-center place-self-center text-center">
 
-                    <h1 className="text-6xl font-bold tracking-widest leading-tight">THE WORLD IS SALTIER</h1>
-                    <h1 className="text-6xl font-bold tracking-widest leading-tight">THAN YOU THOUGHT</h1>
-                    <h3 className="text-2xl font-light tracking-[2rem]">SALTY PROJECT</h3>
+                <div className="mt-10">
+                    <div>
+                        <h2 className="text-3xl font-bold uppercase mb-2">Tech Stack</h2>
+                        <Techstack items={dailyTechStack} />
+                    </div>
+                    <div className="mt-4">
+                        <h3 className="text-2xl font-bold uppercase mb-2">Recent Tech stack</h3>
+                        <Techstack items={recentTechStack} />
+                    </div>
                 </div>
-                <p className="text-center tracking-[1rem] leading-[1_75] font-light text-lg p-4">MINDLESS CREATURE IN THE WORLD OF MADNESS</p>
             </div>
         </div>
     );
