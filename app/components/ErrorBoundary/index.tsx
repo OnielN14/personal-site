c271d7e285bce7b064c8da5f4579a6cc831e9fd2
Daniyal Ahmad Rizaldhi <daniyal.ahmadrizaldhi@gmail.com>
2024-02-01 00:21:58 +0700

    adjust ErrorBoundary layout

--- app/components/ErrorBoundary/index.tsx
+++ app/components/ErrorBoundary/index.tsx
@@ -38,7 +38,7 @@ export default function ErrorBoundary() {
                 <h1 className="text-4xl font-bold">Error</h1>
                 <p className="mb-4">{error.message}</p>
                 <p>The stack trace is:</p>
-                <pre className="px-2 py-1 max-w-[600px] overflow-y-scroll overflow-hidden max-h-[300px] text-wrap bg-gray-300 rounded">{error.stack}</pre>
+                <pre className="px-2 py-1 overflow-y-scroll overflow-hidden max-h-[300px] text-wrap bg-gray-300 rounded">{error.stack}</pre>
             </div>
         )
     }
@@ -53,8 +53,10 @@ export default function ErrorBoundary() {
             </head>
             <body className="font-inter">
                 <div className="min-h-dvh flex flex-col items-center justify-center">
-                    {errorElement}
-                    {backButtonElement}
+                    <div className="flex flex-col items-center justify-center max-w-[600px]">
+                        {errorElement}
+                        {backButtonElement}
+                    </div>
                 </div>
                 <Scripts />
                 <LiveReload />
