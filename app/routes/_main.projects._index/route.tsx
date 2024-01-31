f6bb3d6e643ab67ec448cee7dbe7f302bba3086b
Daniyal Ahmad Rizaldhi <daniyal.ahmadrizaldhi@gmail.com>
2024-01-31 19:09:34 +0700

    add loader to projects route

--- app/routes/_main.projects._index/route.tsx
+++ app/routes/_main.projects._index/route.tsx
@@ -1,6 +1,14 @@
+import { useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node"
 import UnderConstruction from "~/components/UnderConstruction";
 
+export const loader = async () => {
+    return json({})
+}
+
 export default function ProjectsIndex() {
+    useLoaderData<typeof loader>()
+
     return (
         <div className="flex items-center justify-center h-dvh pt-[5rem]">
             <UnderConstruction />
