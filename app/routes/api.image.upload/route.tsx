5c5f073cbdd159a450d3d093c06acf7f032ab83d
Daniyal Ahmad Rizaldhi <daniyal.ahmadrizaldhi@gmail.com>
2024-02-12 18:43:03 +0700

    break validation and upload handler implementation into seperate functions

--- app/routes/api.image.upload/route.tsx
+++ app/routes/api.image.upload/route.tsx
@@ -6,32 +6,55 @@ import { MAX_FILE_SIZE, imageSchemaValidation } from "./utils";
 const uploadDir = '/upload'
 const publicUpload = '/public' + uploadDir
 
-const uploadHandler = unstable_createFileUploadHandler({
+interface ValidateImagePayloadParams {
+    throwOnError?: boolean,
+    fieldName?: string
+}
+
+export const validateImagePayload = async (request: Request, {
+    throwOnError,
+    fieldName
+}: ValidateImagePayloadParams = {
+        throwOnError: true
+    }) => {
+    const tempFormData = await request.clone().formData()
+    const imageBlob = tempFormData.get(fieldName ?? 'image') as Blob
+
+    const result = await imageSchemaValidation.safeParseAsync(imageBlob)
+
+    if (throwOnError && !result.success) {
+        throw json({
+            message: result.error.issues[0].message
+        }, 400)
+    }
+
+    return result
+}
+
+export const uploadHandler = unstable_createFileUploadHandler({
     file: ({ filename }) => filename,
     directory: path.join(process.cwd(), publicUpload),
     maxPartSize: MAX_FILE_SIZE,
 })
 
+export const handleSingleUpload = async (request: Request, fieldName: string = 'image') => {
+    const formData = await unstable_parseMultipartFormData(request, uploadHandler)
+
+    const file = formData.get(fieldName) as NodeOnDiskFile
+    const url = new URL(request.url)
+    const pathname = `${uploadDir}/${encodeURIComponent(file.name)}`
+
+    return {
+        fullUrl: `${url.origin}${pathname}`,
+        urlPathname: pathname
+    }
+}
+
 export const action = async ({ request }: ActionFunctionArgs) => {
     return authenticated(request, async () => {
-        const tempFormData = await request.formData()
-        const imageBlob = tempFormData.get('image') as Blob
+        await validateImagePayload(request)
+        const { fullUrl: url } = await handleSingleUpload(request)
 
-        const result = await imageSchemaValidation.safeParseAsync(imageBlob)
-
-        if (!result.success) {
-            return json({
-                message: result.error.issues[0].message
-            }, 400)
-        }
-
-        const formData = await unstable_parseMultipartFormData(request, uploadHandler)
-
-        const file = formData.get('image') as NodeOnDiskFile
-        const url = new URL(request.url)
-
-        return json({
-            url: `${url.origin}${uploadDir}/${encodeURIComponent(file.name)}`
-        })
+        return json({ url })
     })
 }
