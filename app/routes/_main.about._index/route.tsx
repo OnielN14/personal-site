f80f08a410caff91a3dee0ab01946190397e47e2
Daniyal Ahmad Rizaldhi <daniyal.ahmadrizaldhi@gmail.com>
2024-08-06 09:41:35 +0700

    defer about page data

--- app/routes/_main.about._index/route.tsx
+++ app/routes/_main.about._index/route.tsx
@@ -1,5 +1,5 @@
-import { MetaFunction, json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { MetaFunction, defer, json } from "@remix-run/node";
+import { Await, useLoaderData } from "@remix-run/react";
 import {
     getAboutSummary,
     getEmploymentInfo,
@@ -7,27 +7,31 @@ import {
 } from "~/services/personal-info.server";
 import { SocialLinkList } from "../_main._index/Intro";
 import EmploymentInfo from "./EmploymentInfo";
+import { Suspense } from "react";
+import { Skeleton } from "~/components/ui/skeleton";
 
 export const loader = async () => {
-    const employment = (await getEmploymentInfo()).map((v) => ({ ...v }));
+    const employment = getEmploymentInfo()
+        .then((value) => value.map((v) => ({ ...v })))
+        .then((value) =>
+            value.sort((a, b) => {
+                if (
+                    a.employmentInformation.startDate <
+                    b.employmentInformation.startDate
+                )
+                    return 1;
+                else if (
+                    a.employmentInformation.startDate >
+                    b.employmentInformation.startDate
+                )
+                    return -1;
+                return 0;
+            })
+        );
 
-    employment.sort((a, b) => {
-        if (
-            a.employmentInformation.startDate <
-            b.employmentInformation.startDate
-        )
-            return 1;
-        else if (
-            a.employmentInformation.startDate >
-            b.employmentInformation.startDate
-        )
-            return -1;
-        return 0;
-    });
-
-    return json({
-        about: await getAboutSummary(),
-        socials: await getSocials(),
+    return defer({
+        about: getAboutSummary(),
+        socials: getSocials(),
         employment,
     });
 };
@@ -46,17 +50,44 @@ export default function AboutPage() {
     const { about, socials, employment } = useLoaderData<typeof loader>();
 
     return (
-        <div className="container pt-[10rem]">
-            <h1 className="text-5xl font-bold mb-10">About Me</h1>
-            {about.map((v, i) => (
-                <p key={i} className="[&:not(:last-child)]:mb-8">
-                    {v}
-                </p>
-            ))}
+        <Suspense
+            fallback={
+                <div className="grid md:grid-cols-3 gap-3">
+                    <Skeleton className="h-[250px] w-full rounded-md" />
+                    <Skeleton className="h-[250px] w-full rounded-md" />
+                    <Skeleton className="h-[250px] w-full rounded-md" />
+                    <Skeleton className="h-[250px] w-full rounded-md" />
+                    <Skeleton className="h-[250px] w-full rounded-md" />
+                    <Skeleton className="h-[250px] w-full rounded-md" />
+                </div>
+            }
+        >
+            <Await resolve={Promise.all([about, socials, employment])}>
+                {([about, socials, employment]) => (
+                    <>
+                        <div className="container pt-[10rem]">
+                            <h1 className="text-5xl font-bold mb-10">
+                                About Me
+                            </h1>
+                            {about.map((v, i) => (
+                                <p
+                                    key={i}
+                                    className="[&:not(:last-child)]:mb-8"
+                                >
+                                    {v}
+                                </p>
+                            ))}
 
-            <SocialLinkList items={socials} />
+                            <SocialLinkList items={socials} />
 
-            <EmploymentInfo items={employment} className="mt-8" />
-        </div>
+                            <EmploymentInfo
+                                items={employment}
+                                className="mt-8"
+                            />
+                        </div>
+                    </>
+                )}
+            </Await>
+        </Suspense>
     );
 }
